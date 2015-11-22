2119cbd40df978f24d279b94be51c69d259f98b2
Grégory Bataille <gbataille@gmail.com>
2015-11-22 16:27:22 +0100

    starting to build the parser

--- src/GitHUD.hs
+++ src/GitHUD.hs
@@ -3,13 +3,34 @@ module GitHUD (
     ) where
 
 import System.Process (readProcess)
+import Text.Parsec (runParser, Parsec)
+import Text.Parsec.Char (anyChar, newline, noneOf)
+import Text.Parsec.Prim (many)
+import Text.Parsec.Combinator (manyTill)
+import Text.Parsec.Error (ParseError)
+
+type GitParser = Parsec String () String
 
 githud :: IO ()
 githud = do
   -- TODO: gbataille - Check that we are in a git repo
   out <- gitPorcelainStatus
-  print out
+  let parsed = runParser porcelainStatusParser () "" out
+  outputParsed parsed
 
 -- | Assumes that we are in a git repo
 gitPorcelainStatus :: IO String
 gitPorcelainStatus = readProcess "git" ["status", "--porcelain"] ""
+
+porcelainStatusParser :: GitParser
+porcelainStatusParser = concat <$> many gitLines
+
+gitLines :: GitParser
+gitLines = do
+    line <- many $ noneOf "\n"
+    newline
+    return line
+
+outputParsed :: Either ParseError String -> IO ()
+outputParsed (Left error) = print error
+outputParsed (Right str) = print str
