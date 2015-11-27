c22f551c3bd965f334dceef5b380ce708e6c9405
Grégory Bataille <gbataille@gmail.com>
2015-11-27 14:15:54 +0100

    Reorg code

--- src/GitHUD.hs
+++ src/GitHUD.hs
@@ -4,13 +4,13 @@ module GitHUD (
 
 import System.Process (readProcess)
 import System.Console.ANSI (setSGR, SGR(Reset,SetColor), ConsoleLayer(..), ColorIntensity(..), Color(..))
-import GitHUD.Parse
+import GitHUD.Parse.Status
 
 githud :: IO ()
 githud = do
   -- TODO: gbataille - Check that we are in a git repo
   out <- gitPorcelainStatus
-  let repoState = gitParse out
+  let repoState = gitParseStatus out
   outputRepoState repoState
 
 -- | Assumes that we are in a git repo
