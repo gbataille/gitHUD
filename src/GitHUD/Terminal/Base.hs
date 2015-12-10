1bbf570e557dd5dcea210bab72312ffc38fd9647
Grégory Bataille <gbataille@gmail.com>
2015-12-10 22:42:54 +0100

    zsh special color character handling

--- src/GitHUD/Terminal/Base.hs
+++ src/GitHUD/Terminal/Base.hs
@@ -1,26 +1,37 @@
-{-# LANGUAGE MultiParamTypeClasses #-}
-
 module GitHUD.Terminal.Base (
   showStrInColor
-  -- , Color(..)
-  -- , ColorIntensity(..)
-  -- , TerminalColorCoder
   ) where
 
-import qualified System.Console.ANSI as ANSI
+import GitHUD.Terminal.Types (Color(..), ColorIntensity(..))
 
-import GitHUD.Terminal.ANSI (outputInANSITerminalColor)
-
--- data Color = Black | Red | Green | Yellow | Blue | Magenta | Cyan | White
--- data ColorIntensity = Dull | Vivid
---
--- class TerminalColorCoder where
---   codeStartFromColor :: Color -> String
---   codeEndFromColor :: Color -> String
-
-showStrInColor :: ANSI.Color
-               -> ANSI.ColorIntensity
+showStrInColor :: Color
+               -> ColorIntensity
                -> String
                -> IO ()
 showStrInColor color intensity str = do
-  outputInANSITerminalColor color intensity str
+  putStr $ terminalStartCode color intensity
+  putStr str
+  putStr terminalEndCode
+
+terminalStartCode :: Color
+                  -> ColorIntensity
+                  -> String
+terminalStartCode  Black    Vivid  = "%{\x1b[1;30m%}"
+terminalStartCode  Red      Vivid  = "%{\x1b[1;31m%}"
+terminalStartCode  Green    Vivid  = "%{\x1b[1;32m%}"
+terminalStartCode  Yellow   Vivid  = "%{\x1b[1;33m%}"
+terminalStartCode  Blue     Vivid  = "%{\x1b[1;34m%}"
+terminalStartCode  Magenta  Vivid  = "%{\x1b[1;35m%}"
+terminalStartCode  Cyan     Vivid  = "%{\x1b[1;36m%}"
+terminalStartCode  White    Vivid  = "%{\x1b[1;37m%}"
+terminalStartCode  Black    Dull   = "%{\x1b[30m%}"
+terminalStartCode  Red      Dull   = "%{\x1b[31m%}"
+terminalStartCode  Green    Dull   = "%{\x1b[32m%}"
+terminalStartCode  Yellow   Dull   = "%{\x1b[33m%}"
+terminalStartCode  Blue     Dull   = "%{\x1b[34m%}"
+terminalStartCode  Magenta  Dull   = "%{\x1b[35m%}"
+terminalStartCode  Cyan     Dull   = "%{\x1b[36m%}"
+terminalStartCode  White    Dull   = "%{\x1b[37m%}"
+
+terminalEndCode :: String
+terminalEndCode = "%{\x1b[0m%}"
---
+++ src/GitHUD/Terminal/Types.hs
@@ -0,0 +1,7 @@
+module GitHUD.Terminal.Types (
+  Color(..)
+  , ColorIntensity(..)
+  ) where
+
+data Color = Black | Red | Green | Yellow | Blue | Magenta | Cyan | White
+data ColorIntensity = Dull | Vivid
