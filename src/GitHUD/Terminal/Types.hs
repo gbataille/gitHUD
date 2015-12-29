e1854fcd6ba9c6e69ec0213ddf6ff8939f08e8e4
Grégory Bataille <gregory.bataille@gmail.com>
2015-12-29 18:14:41 +0100

    [Feature] Add a Prompt state data type

--- src/GitHUD/Terminal/Types.hs
+++ src/GitHUD/Terminal/Types.hs
@@ -2,7 +2,8 @@ module GitHUD.Terminal.Types (
   Color(..)
   , ColorIntensity(..)
   , Shell(..)
-  , ShellOutput(..)
+  , Prompt(..)
+  , ShellOutput
   ) where
 
 import Control.Monad.Reader
@@ -10,5 +11,8 @@ import Control.Monad.Reader
 data Color = Black | Red | Green | Yellow | Blue | Magenta | Cyan | White
 data ColorIntensity = Dull | Vivid
 data Shell = ZSH | Other deriving (Eq)
+
+data Prompt = Prompt { shellType :: Shell, prompt :: String }
+
 type ShellOutput = ReaderT Shell IO ()
 
