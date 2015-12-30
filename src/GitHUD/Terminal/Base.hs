5bc71982903885a4b0c89155544f774d1920f3fc
Grégory Bataille <gregory.bataille@gmail.com>
2015-12-30 09:37:52 +0100

    [Feature] Terminal.Base to work on string, not IO

--- src/GitHUD/Terminal/Base.hs
+++ src/GitHUD/Terminal/Base.hs
@@ -1,59 +1,64 @@
 module GitHUD.Terminal.Base (
-  -- showStrInColor
+  addColorToStringForShell
   ) where
 
-import Control.Monad.State
-import Data.Monoid (mappend)
+import Control.Monad.Reader (ask)
+import Control.Monad.Writer (tell)
 
 import GitHUD.Terminal.Types
 
--- showStrInColor :: Color               -- ^ The terminal color to use
---                -> ColorIntensity      -- ^ The intensity to use
---                -> String              -- ^ The string to output
---                -> ShellOutput
--- showStrInColor color intensity str = do
---   shell <- get
---   liftIO $ outputStrInColor color intensity str (shellType shell)
---
--- outputStrInColor :: Color
---                  -> ColorIntensity
---                  -> String
---                  -> Shell
---                  -> IO()
--- outputStrInColor color intensity str shell = do
---   let startCode = terminalStartCode color intensity
---   if (shell == ZSH)
---     then putStr $ zshMarkZeroWidth startCode
---     else putStr $ startCode
---
---   putStr str
---   if (shell == ZSH)
---     then putStr $ zshMarkZeroWidth terminalEndCode
---     else putStr $ terminalEndCode
---
--- zshMarkZeroWidth :: String
---                  -> String
--- zshMarkZeroWidth str = "%{" `mappend` str `mappend` "%}"
---
--- terminalStartCode :: Color
---                   -> ColorIntensity
---                   -> String
--- terminalStartCode  Black    Vivid  = "\x1b[1;30m"
--- terminalStartCode  Red      Vivid  = "\x1b[1;31m"
--- terminalStartCode  Green    Vivid  = "\x1b[1;32m"
--- terminalStartCode  Yellow   Vivid  = "\x1b[1;33m"
--- terminalStartCode  Blue     Vivid  = "\x1b[1;34m"
--- terminalStartCode  Magenta  Vivid  = "\x1b[1;35m"
--- terminalStartCode  Cyan     Vivid  = "\x1b[1;36m"
--- terminalStartCode  White    Vivid  = "\x1b[1;37m"
--- terminalStartCode  Black    Dull   = "\x1b[30m"
--- terminalStartCode  Red      Dull   = "\x1b[31m"
--- terminalStartCode  Green    Dull   = "\x1b[32m"
--- terminalStartCode  Yellow   Dull   = "\x1b[33m"
--- terminalStartCode  Blue     Dull   = "\x1b[34m"
--- terminalStartCode  Magenta  Dull   = "\x1b[35m"
--- terminalStartCode  Cyan     Dull   = "\x1b[36m"
--- terminalStartCode  White    Dull   = "\x1b[37m"
---
--- terminalEndCode :: String
--- terminalEndCode = "\x1b[0m"
+addColorToStringForShell :: Color               -- ^ The terminal color to use
+                         -> ColorIntensity      -- ^ The intensity to use
+                         -> String              -- ^ The string to output
+                         -> ShellOutput
+addColorToStringForShell color intensity str = do
+  outputConfig <- ask
+  let shell = getShell outputConfig
+  tell $ startColorMarker color intensity shell
+  tell $ str
+  tell $ endColorMarker shell
+
+startColorMarker :: Color
+                 -> ColorIntensity
+                 -> Shell
+                 -> String
+startColorMarker color intensity shell =
+  applyShellMarkers shell $ terminalStartCode color intensity
+
+endColorMarker :: Shell
+               -> String
+endColorMarker shell =
+  applyShellMarkers shell $ terminalEndCode
+
+applyShellMarkers :: Shell
+                  -> String
+                  -> String
+applyShellMarkers ZSH = zshMarkZeroWidth
+applyShellMarkers _ = id
+
+zshMarkZeroWidth :: String
+                 -> String
+zshMarkZeroWidth str = "%{" `mappend` str `mappend` "%}"
+
+terminalStartCode :: Color
+                  -> ColorIntensity
+                  -> String
+terminalStartCode  Black    Vivid  = "\x1b[1;30m"
+terminalStartCode  Red      Vivid  = "\x1b[1;31m"
+terminalStartCode  Green    Vivid  = "\x1b[1;32m"
+terminalStartCode  Yellow   Vivid  = "\x1b[1;33m"
+terminalStartCode  Blue     Vivid  = "\x1b[1;34m"
+terminalStartCode  Magenta  Vivid  = "\x1b[1;35m"
+terminalStartCode  Cyan     Vivid  = "\x1b[1;36m"
+terminalStartCode  White    Vivid  = "\x1b[1;37m"
+terminalStartCode  Black    Dull   = "\x1b[30m"
+terminalStartCode  Red      Dull   = "\x1b[31m"
+terminalStartCode  Green    Dull   = "\x1b[32m"
+terminalStartCode  Yellow   Dull   = "\x1b[33m"
+terminalStartCode  Blue     Dull   = "\x1b[34m"
+terminalStartCode  Magenta  Dull   = "\x1b[35m"
+terminalStartCode  Cyan     Dull   = "\x1b[36m"
+terminalStartCode  White    Dull   = "\x1b[37m"
+
+terminalEndCode :: String
+terminalEndCode = "\x1b[0m"
