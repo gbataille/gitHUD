63f030e4f864df2e1c02f875a3265e46e82f550d
Grégory Bataille <gregory.bataille@gmail.com>
2016-01-02 20:13:18 +0100

    [Feature] Config file optional and in user directory

--- src/GitHUD.hs
+++ src/GitHUD.hs
@@ -5,8 +5,11 @@ module GitHUD (
 import Control.Monad (when)
 import Control.Monad.Reader (runReader)
 import System.Environment (getArgs)
+import System.Posix.Files (fileExist)
+import System.Posix.User (getRealUserID, getUserEntryForID, UserEntry(..))
 
 import GitHUD.Config.Parse
+import GitHUD.Config.Types
 import GitHUD.Terminal.Prompt
 import GitHUD.Terminal.Types
 import GitHUD.Git.Parse.Base
@@ -15,14 +18,11 @@ import GitHUD.Types
 
 githud :: IO ()
 githud = do
-  shell <- processArguments getArgs
-
-  -- TODO: gbataille - change that to the home folder
-  -- TODO: gbataille - Error if the file does not exists. Have to check first
-  config <- parseConfigFile "/Users/gbataille/Documents/Prog/Perso/gitHUD/.githudrc"
-
+  -- Exit ASAP if we are not in a git repository
   isGit <- checkInGitDirectory
   when isGit $ do
+    shell <- processArguments getArgs
+    config <- getAppConfig
     repoState <- getGitRepoState
     let prompt = runReader buildPromptWithConfig $ buildOutputConfig shell repoState config
 
@@ -37,3 +37,12 @@ processArguments args = do
     then return ZSH
     else return Other
 
+getAppConfig :: IO Config
+getAppConfig = do
+  uid <- getRealUserID
+  userEntry <- getUserEntryForID uid
+  let configFilePath = (homeDirectory userEntry) ++ "/.githudrc"
+  configFilePresent <- fileExist configFilePath
+  if configFilePresent
+    then parseConfigFile configFilePath
+    else return defaultConfig
