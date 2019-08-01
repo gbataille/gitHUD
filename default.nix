d72057317dfc697889e8e5742774454288d4041e
Grégory Bataille <gregory.bataille@gmail.com>
2019-08-01 09:07:57 +0200

    chore(): split nix file for easier use - repl

--- default.nix
+++ default.nix
@@ -1,7 +1,5 @@
-with import (fetchTarball {
-   url = https://github.com/nixos/nixpkgs/archive/88d9f776091.tar.gz;
-   sha256 = "0z8a0g69fmbbzi77jhvhwafv73dn5fg3gsr0q828lss6j5qpx995";
-}) {};
+{ nixpkgs ? ./nixpkgs.nix }:
+with import nixpkgs {};
 
 haskellPackages.developPackage {
   root = ./.;
