98d0d46ac4325ea488b9e67fc521e053b8ec88e7
Grégory Bataille <gregory.bataille@gmail.com>
2019-08-04 18:21:11 +0200

    feat(): call githudd from githud

--- src/GitHUD/Daemon/Runner.hs
+++ src/GitHUD/Daemon/Runner.hs
@@ -9,8 +9,6 @@ import qualified Control.Exception as E
 import Control.Monad (when, forever, void, unless)
 import qualified Data.ByteString.UTF8 as BSU
 import Data.Maybe (fromMaybe)
-import Network.Socket (Family(AF_UNIX), socket, defaultProtocol, SocketType(Stream), close, listen, accept, bind, SockAddr(SockAddrUnix), connect)
-import Network.Socket.ByteString (recv, sendAll)
 import System.Directory (removeFile)
 import System.Posix.Daemon (isRunning, runDetached, Redirection(DevNull, ToFile))
 import System.Posix.Files (fileExist)
