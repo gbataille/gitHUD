0383ebeb468e30b2729bb2db8ac896a08341afeb
Grégory Bataille <gregory.bataille@gmail.com>
2020-01-31 12:20:49 +0100

    feat: debug only when GITHUD_DEBUG set to TRUE

--- src/GitHUD/Debug.hs
+++ src/GitHUD/Debug.hs
@@ -3,16 +3,36 @@ module GitHUD.Debug (
   debugOnStderr
   ) where
 
+import Control.Monad (when)
+import Data.Maybe (fromMaybe)
+import System.Environment (lookupEnv)
 import System.IO (stdout, stderr, hFlush, hPutStrLn)
+
+debugEnvVar :: String
+debugEnvVar = "GITHUD_DEBUG"
+
+debugEnvVarValue :: String
+debugEnvVarValue = "TRUE"
+
+isDebugActive :: IO Bool
+isDebugActive = do
+  env <- lookupEnv debugEnvVar
+  let debug = fromMaybe "FALSE" env
+  return $ debug == debugEnvVarValue
+
+whenDebugActive :: IO () -> IO ()
+whenDebugActive effect = do
+  debugActive <- isDebugActive
+  if debugActive then effect else return ()
 
 debug :: String
       -> IO ()
-debug msg = do
+debug msg = whenDebugActive $ do
   putStrLn msg
   hFlush stdout
 
 debugOnStderr :: String
               -> IO ()
-debugOnStderr msg = do
+debugOnStderr msg = whenDebugActive $ do
   hPutStrLn stderr msg
   hFlush stderr
