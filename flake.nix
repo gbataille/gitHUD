59a50c01d7335647e02d82039dddc6daded4a2ec
Simon Kohlmeyer <simon.kohlmeyer@gmail.com>
2022-09-25 04:47:20 +0200

    Relax version bounds to make this compatible with recent nixpkgs

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "githud flake";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs?rev=0bf298df24f721a7f85c580339fb7eeff64b927c";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
